f1ab0f2e17970c7ad9c808add09029f65ee12b59
Mason Rhodes <mason@lifemd.com>
2024-10-23 10:14:08 -0700

    feat: encryption

--- software_installers/07_vpn.sh
+++ software_installers/07_vpn.sh
@@ -4,7 +4,16 @@ set -eu
 # PriTunl
 if ! command -v pritunl-client &> /dev/null; then
     echo "Prepping VPN"
-    xdg-open https://vpn.lifemd.io/sso/request
+    sudo bash -c 'cat <<EOF > /etc/yum.repos.d/pritunl.repo
+[pritunl]
+name=Pritunl Stable Repository
+baseurl=https://repo.pritunl.com/stable/yum/fedora/39/
+gpgcheck=1
+enabled=1
+EOF'
+    
+    sudo dnf install -y pritunl-client-electron
 fi
+
 
 exit 0
