88d7042d209fa84aedb96b3519bda523a5ebb308
Mason Rhodes <mason@lifemd.com>
2025-08-20 09:47:00 -0700

    Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/user/alpha.lua Add .config/nvim/lua/user/autocmds.lua Add .config/nvim/lua/user/autopairs.lua Add .config/nvim/lua/user/breadcrumbs.lua Add .config/nvim/lua/user/cmp.lua Add .config/nvim/lua/user/colorscheme.lua Add .config/nvim/lua/user/comment.lua Add .config/nvim/lua/user/devicons.lua Add .config/nvim/lua/user/extras/bqf.lua Add .config/nvim/lua/user/extras/cellular-automaton.lua Add .config/nvim/lua/user/extras/colorizer.lua Add .config/nvim/lua/user/extras/copilot.lua Add .config/nvim/lua/user/extras/dressing.lua Add .config/nvim/lua/user/extras/eyeliner.lua Add .config/nvim/lua/user/extras/fidget.lua Add .config/nvim/lua/user/extras/gitlinker.lua Add .config/nvim/lua/user/extras/lab.lua Add .config/nvim/lua/user/extras/modicator.lua Add .config/nvim/lua/user/extras/navbuddy.lua Add .config/nvim/lua/user/extras/neoscroll.lua Add .config/nvim/lua/user/extras/neotab.lua Add .config/nvim/lua/user/extras/oil.lua Add .config/nvim/lua/user/extras/tabby.lua Add .config/nvim/lua/user/extras/tabnine.lua Add .config/nvim/lua/user/extras/ufo.lua Add .config/nvim/lua/user/gitsigns.lua Add .config/nvim/lua/user/harpoon.lua Add .config/nvim/lua/user/hyprlang.lua Add .config/nvim/lua/user/icons.lua Add .config/nvim/lua/user/illuminate.lua Add .config/nvim/lua/user/indentline.lua Add .config/nvim/lua/user/keymaps.lua Add .config/nvim/lua/user/launch.lua Add .config/nvim/lua/user/lazy.lua Add .config/nvim/lua/user/lspconfig.lua Add .config/nvim/lua/user/lspsettings/jsonls.lua Add .config/nvim/lua/user/lspsettings/lua_ls.lua Add .config/nvim/lua/user/lualine.lua Add .config/nvim/lua/user/mason.lua Add .config/nvim/lua/user/navic.lua Add .config/nvim/lua/user/neogit.lua Add .config/nvim/lua/user/neotest.lua Add .config/nvim/lua/user/none-ls.lua Add .config/nvim/lua/user/nvimtree.lua Add .config/nvim/lua/user/options.lua Add .config/nvim/lua/user/project.lua Add .config/nvim/lua/user/schemastore.lua Add .config/nvim/lua/user/telescope.lua Add .config/nvim/lua/user/toggleterm.lua Add .config/nvim/lua/user/treesitter.lua Add .config/nvim/lua/user/whichkey.lua Remove .config/systemd/user/kanshi.service Update .wezterm.lua

---
+++ dot_config/nvim/lua/user/mason.lua
@@ -0,0 +1,31 @@
+local M = {
+  "williamboman/mason-lspconfig.nvim",
+  commit = "56e435e09f8729af2d41973e81a0db440f8fe9c9", -- Compatible with Neovim 0.10.4
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+}
+
+function M.config()
+  local servers = {
+    "lua_ls",
+    "cssls",
+    "html",
+    "tsserver",
+    "pyright",
+    "bashls",
+    "jsonls",
+  }
+
+  require("mason").setup {
+    ui = {
+      border = "rounded",
+    },
+  }
+
+  require("mason-lspconfig").setup {
+    ensure_installed = servers,
+  }
+end
+
+return M
