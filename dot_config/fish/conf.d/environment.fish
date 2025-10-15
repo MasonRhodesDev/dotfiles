d352de647689e48d10399deadebe3037c18e22bf
Mason Rhodes <mason@lifemd.com>
2025-10-15 10:09:07 -0700

    Update .config/fish/conf.d/environment.fish Add .config/fish/conf.d/theme-colors.fish Update .config/fish/fish_variables

--- dot_config/fish/conf.d/environment.fish
+++ dot_config/fish/conf.d/environment.fish
@@ -1,16 +1,22 @@
 # Source shared POSIX environment variables
 if test -f ~/.config/environment
     # Parse and export variables from POSIX format
-    for line in (cat ~/.config/environment | grep -v '^#' | grep -v '^$' | grep 'export')
-        set -l clean_line (string replace 'export ' '' $line)
+    for line in (cat ~/.config/environment | grep 'export' | grep -v '^#' | grep -v '^$')
+        # Trim leading/trailing whitespace
+        set -l clean_line (string trim $line)
+        set -l clean_line (string replace 'export ' '' $clean_line)
         set -l parts (string split -m 1 '=' $clean_line)
         if test (count $parts) -eq 2
-            # Remove quotes if present
-            set -l value (string trim -c '"' $parts[2])
-            # Expand $HOME and other variables
-            set -l expanded_value (string replace -a '$HOME' $HOME $value)
-            set -l expanded_value (string replace -a '$PATH' $PATH $expanded_value)
-            set -gx $parts[1] $expanded_value
+            set -l var_name (string trim $parts[1])
+            # Skip if variable name is empty or contains spaces
+            if test -n "$var_name"; and not string match -q '* *' "$var_name"
+                # Remove quotes if present
+                set -l value (string trim -c '"' $parts[2])
+                # Expand $HOME and other variables
+                set -l expanded_value (string replace -a '$HOME' $HOME $value)
+                set -l expanded_value (string replace -a '$PATH' $PATH $expanded_value)
+                set -gx $var_name $expanded_value
+            end
         end
     end
 end
